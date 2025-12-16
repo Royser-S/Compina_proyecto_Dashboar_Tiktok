bff9c00b5c3a2e656224d574f9b9937263f8c0a2
Royser Chuquihuayta Aranda <royserfonce@gmail.com>
2025-12-16 09:11:08 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -47,7 +47,7 @@ function App() {
     formData.append('username', '@compipro');
 
     try {
-      const response = await axios.post('http://127.0.0.1:5000/api/upload', formData, {
+      const response = await axios.post('/api/upload', formData, {
         headers: { 'Content-Type': 'multipart/form-data' }
       });
       const responseData = response.data;
